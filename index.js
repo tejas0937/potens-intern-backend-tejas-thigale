4379d4110d2ebde353be568f95f53b6f75a794a3
tejas0937 <tejsthigale0937@gmail.com>
2026-05-21 19:25:53 +0530

    Set index.js logic which will serve as entry point of our app
    all our log routes are mounted here and exceptions are handled

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
  
+const express = require('express');
+const dotenv = require('dotenv');
+const pino = require('pino');
+const logRoutes = require('./src/routes/log.routes');
+
+dotenv.config();
+
+const app = express();
+const logger = pino({ transport: { target: 'pino-pretty' } });
+
+// parse incoming JSON request bodies
+app.json());
+
+// all our log routes are mounted here
+app.use('/', logRoutes);
+
+// handle routes that dont exist
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+// global error handler, catches anything unexpected
+app.use((err, req, res, next) => {
+  logger.error(err, 'unhandled error');
+  res.status(500).json({ error: 'Something went wrong' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  logger.info(`server is running on port ${PORT}`);
+});
